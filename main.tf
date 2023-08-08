a00e6902c9abe4d8c9669c119d1a1b51076ae656
AlexTzk <138780223+AlexTzk@users.noreply.github.com>
2023-08-08 17:06:48 +0100

    SQL module for PostgreSQL
    Single instance in HA (REGIONAL) or non-HA (ZONAL) controlled via a tfvars file. Configured to be on a private network not exposed to the outside world.

--- main.tf
+++ main.tf
@@ -32,6 +32,7 @@ module "network" {
   region                                 = "${var.region}"
   pub_subnet_cidr                        = "${var.pub_subnet_cidr}"
   pri_subnet_cidr                        = "${var.pri_subnet_cidr}"
+  pri_vpc_peering_address                = "${var.pri_vpc_peering_address}"
 }
 # Firewall
 module "firewall" {
@@ -42,10 +43,28 @@ module "firewall" {
   pri_subnet_cidr                        = "${var.pri_subnet_cidr}"
   office_ip                              = "${var.office_ip}"
   failsafe_ip                            = "${var.failsafe_ip}"
+  depends_on                             = [module.network]
 }
 # SQL
 module "sql" {
   source                                 = "./modules/sql"
   company                                = "${var.company}"
-  
+  project 				                       = "${var.project}"
+  env                                    = "${var.env}"
+  region                                 = "${var.region}"
+  zone                                   = "${var.zone}"
+  db_tier                                = "${var.db_tier}"
+  db_availability_type                   = "${var.db_availability_type}"
+  db_version                             = "${var.db_version}"
+  db_disk_size                           = "${var.db_disk_size}"
+  db_disk_type                           = "${var.db_disk_type}"
+  db_deletion                            = "${var.db_deletion}"
+  db_point_recovery                      = "${var.db_point_recovery}"
+  db_name                                = "${var.db_name}"
+  db_user_1                              = "${var.db_user_1}"
+  db_password_1                          = "${var.db_password_1}"
+  db_user_2                              = "${var.db_user_2}"
+  db_password_2                          = "${var.db_password_2}"
+  network_id                             =  module.network.network_id
+  depends_on                             = [module.network]
 }
